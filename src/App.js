94e7d065c727a16388cf934f89f4286fba95a744
Antonio Rodrigues Coutinho <antoniorodrigues.coutinho@gmail.com>
2018-01-28 14:41:44 -0300

    refactor: Replacing ternary function to Route component

--- src/App.js
+++ src/App.js
@@ -3,31 +3,30 @@ import React from 'react'
 import './App.css'
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
+import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
 
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
-  addBook = () =>{
-    this.setState({showSearchPage: true})
+  addBook = () => {
+    this.setState({ showSearchPage: true })
   }
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks/>
-        ) : (
-          <ListBooks AddBook={this.addBook}/>
+        <Route
+          exact path='/'
+          render={() => (
+            <ListBooks
+              AddBook={()=>{this.addBook}} />
+          )}
+        />
+        <Route
+          path='/search'
+          render={() => (
+            <SearchBooks />
+          )}
+        />
 
-        )}
       </div>
     )
   }
