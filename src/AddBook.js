887761b088c5ff57be152a6d55513573d08d8950
Antonio Rodrigues Coutinho <antoniorodrigues.coutinho@gmail.com>
2018-01-29 19:46:24 -0300

    fix: Fix Link in AddBook and SearchBooks

--- src/AddBook.js
+++ src/AddBook.js
@@ -1,11 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class AddBook extends Component {
     render() {
-        return(
+        return (
             <div className="open-search">
-            {/* Receving propsAddBook from App.js. */}
-              <a onClick={this.props.AddBook}>Add a book</a>
+                {/* Receving propsAddBook from App.js. */}
+                <Link
+                    to='/search'
+                >Open Search
+                </Link>
             </div>
         )
     }
--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class BooksApp extends React.Component {
           exact path='/'
           render={() => (
             <ListBooks
-              AddBook={()=>{this.addBook}} />
+              AddBook={this.addBook} />
           )}
         />
         <Route
